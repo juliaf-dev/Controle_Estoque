01cbc11752a39732cf45ef9173b33344d8cb871b
Joaomachado19 <joaovwmachado@gmail.com>
2025-06-29 19:54:38 -0300

    Adicionado os novos campos nas rotas

--- src/models/Fornecedor.ts
+++ src/models/Fornecedor.ts
@@ -4,41 +4,53 @@ import { Sequelize, Op } from 'sequelize';
 interface FornecedorAttributes {
     id: number;
     nome: string;
-    situacao: number
+    situacao: number;
+    email: string;
+    telefone: string;
+    tempo_entrega: number;
 }
 
-interface FornecedorCreationAttributes
-    extends Optional<FornecedorAttributes, 'id'> {}
+interface FornecedorCreationAttributes extends Optional<FornecedorAttributes, 'id'> {}
 
 class Fornecedor extends Model<FornecedorAttributes, FornecedorCreationAttributes>
     implements FornecedorAttributes {
         public id!: number;
         public nome!: string;
         public situacao!: number;
+        public email!: string;
+        public telefone!: string;
+        public tempo_entrega!: number;
 
-    static async registrar(dados: { nome: string; situacao: number }) {
+    static async registrar(dados: { nome: string; situacao: number; email: string; telefone: string; tempo_entrega: number }) {
         return await Fornecedor.create({
             nome: dados.nome,
-            situacao: dados.situacao
+            situacao: dados.situacao,
+            email: dados.email,
+            telefone: dados.telefone,
+            tempo_entrega: dados.tempo_entrega
         });
     }
-    static async atualizar(dados: { id: number ,nome: string; situacao: number }) {
+
+    static async atualizar(dados: { id: number; nome: string; situacao: number; email: string; telefone: string; tempo_entrega: number }) {
         return await Fornecedor.update(
             {
                 nome: dados.nome,
-                situacao: dados.situacao
+                situacao: dados.situacao,
+                email: dados.email,
+                telefone: dados.telefone,
+                tempo_entrega: dados.tempo_entrega
             },
             {
                 where: { id: dados.id }
             }
         );
     }
+
     static async apagar(id: number) {
         return await Fornecedor.destroy({
             where: { id }
         });
     }
-        
 }
 
 export function initFornecedorModel(sequelize: Sequelize) {
@@ -57,6 +69,19 @@ export function initFornecedorModel(sequelize: Sequelize) {
                 type: DataTypes.INTEGER,
                 allowNull: false,
                 defaultValue: 1
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            telefone: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            tempo_entrega: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+                defaultValue: 7
             }
         },
         {
